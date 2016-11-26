15a68b77e1250e4a080ab804bfdd7065e68d50f4
Alex Gao <alexgao@Alexs-MacBook-Air.local>
2016-11-25 19:50:20 -0500

    Updated

--- TestingNet.py
+++ TestingNet.py
@@ -128,8 +128,6 @@ def build_cnn(input_var=None):
     # Input layer, as usual:
     network = lasagne.layers.InputLayer(shape=(None, 1, 128, 128),
                                         input_var=input_var)
-    # This time we do not apply input dropout, as it tends to work less well
-    # for convolutional layers.
 
     # Convolutional layer with 32 kernels of size 5x5. Strided and padded
     # convolutions are supported as well; see the docstring.
@@ -137,9 +135,6 @@ def build_cnn(input_var=None):
             network, num_filters=32, filter_size=(5, 5),
             nonlinearity=lasagne.nonlinearities.rectify,
             W=lasagne.init.GlorotUniform())
-    # Expert note: Lasagne provides alternative convolutional layers that
-    # override Theano's choice of which implementation to use; for details
-    # please see http://lasagne.readthedocs.org/en/latest/user/tutorial.html.
 
     # Max-pooling layer of factor 2 in both dimensions:
     network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
@@ -151,10 +146,10 @@ def build_cnn(input_var=None):
     network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
 
     # A fully-connected layer of 256 units with 50% dropout on its inputs:
-    network = lasagne.layers.DenseLayer(
-            lasagne.layers.dropout(network, p=.5),
-            num_units=256,
-            nonlinearity=lasagne.nonlinearities.rectify)
+    #network = lasagne.layers.DenseLayer(
+    #        lasagne.layers.dropout(network, p=.5),
+    #        num_units=256,
+    #        nonlinearity=lasagne.nonlinearities.rectify)
 
     # And, finally, the 8-unit output layer with 50% dropout on its inputs:
     network = lasagne.layers.DenseLayer(
@@ -198,7 +193,7 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
 # more functions to better separate the code, but it wouldn't make it any
 # easier to read.
 
-def main(model='custom_mlp:1,800,0.2,0.5', num_epochs=200):
+def main(model='cnn', num_epochs=200):
     # Load the dataset
     print("Loading data...")
     # WE CAN CHANGE BELOW ####################################################################################
